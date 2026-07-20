1caaf4d9b0f3d402615a967832d55837c95c3e32
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2026-07-20 23:39:46 +0200

    Adjust SqlGenerationTest for cb42647

--- src/test/java/de/fabiexe/sjql/SqlGenerationTest.java
+++ src/test/java/de/fabiexe/sjql/SqlGenerationTest.java
@@ -11,24 +11,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SqlGenerationTest {
     @Test
     public void testLogicalOperators() {
-        assertEquals("(name) = (?)", SQLUtil.buildSql(Coffee.NAME.eq("Espresso")).getKey());
-        assertEquals("(name) != (?)", SQLUtil.buildSql(Coffee.NAME.neq("Espresso")).getKey());
-        assertEquals("(price) > (?)", SQLUtil.buildSql(Coffee.PRICE.gt(2.0)).getKey());
-        assertEquals("(price) >= (?)", SQLUtil.buildSql(Coffee.PRICE.gte(2.5)).getKey());
-        assertEquals("(price) < (?)", SQLUtil.buildSql(Coffee.PRICE.lt(3.0)).getKey());
-        assertEquals("(price) <= (?)", SQLUtil.buildSql(Coffee.PRICE.lte(2.5)).getKey());
-        assertEquals("((name) = (?)) AND ((price) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").and(Coffee.PRICE.gt(2.0))).getKey());
-        assertEquals("((name) = (?)) OR ((price) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").or(Coffee.PRICE.gt(2.0))).getKey());
-        assertEquals("((name) = (?)) XOR ((price) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").xor(Coffee.PRICE.gt(2.0))).getKey());
-        assertEquals("(NOT ((name) = (?)))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").not()).getKey());
+        assertEquals("(`name`) = (?)", SQLUtil.buildSql(Coffee.NAME.eq("Espresso")).getKey());
+        assertEquals("(`name`) != (?)", SQLUtil.buildSql(Coffee.NAME.neq("Espresso")).getKey());
+        assertEquals("(`price`) > (?)", SQLUtil.buildSql(Coffee.PRICE.gt(2.0)).getKey());
+        assertEquals("(`price`) >= (?)", SQLUtil.buildSql(Coffee.PRICE.gte(2.5)).getKey());
+        assertEquals("(`price`) < (?)", SQLUtil.buildSql(Coffee.PRICE.lt(3.0)).getKey());
+        assertEquals("(`price`) <= (?)", SQLUtil.buildSql(Coffee.PRICE.lte(2.5)).getKey());
+        assertEquals("((`name`) = (?)) AND ((`price`) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").and(Coffee.PRICE.gt(2.0))).getKey());
+        assertEquals("((`name`) = (?)) OR ((`price`) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").or(Coffee.PRICE.gt(2.0))).getKey());
+        assertEquals("((`name`) = (?)) XOR ((`price`) > (?))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").xor(Coffee.PRICE.gt(2.0))).getKey());
+        assertEquals("(NOT ((`name`) = (?)))", SQLUtil.buildSql(Coffee.NAME.eq("Espresso").not()).getKey());
     }
 
     @Test
     public void testIsNullAndIsNotNull() {
-        assertEquals("(name) IS NULL", SQLUtil.buildSql(Coffee.NAME.checkNull()).getKey());
-        assertEquals("(name) IS NOT NULL", SQLUtil.buildSql(Coffee.NAME.checkNotNull()).getKey());
-        assertEquals("(name) IS NULL", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.checkNull()));
-        assertEquals("(name) IS NOT NULL", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.checkNotNull()));
+        assertEquals("(`name`) IS NULL", SQLUtil.buildSql(Coffee.NAME.checkNull()).getKey());
+        assertEquals("(`name`) IS NOT NULL", SQLUtil.buildSql(Coffee.NAME.checkNotNull()).getKey());
+        assertEquals("(`name`) IS NULL", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.checkNull()));
+        assertEquals("(`name`) IS NOT NULL", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.checkNotNull()));
     }
 
     @Test
@@ -39,18 +39,18 @@ public class SqlGenerationTest {
 
     @Test
     public void testColumnToColumnComparison() {
-        assertEquals("(name) = (name)", SQLUtil.buildSql(Coffee.NAME.eq(Coffee.NAME)).getKey());
+        assertEquals("(`name`) = (`name`)", SQLUtil.buildSql(Coffee.NAME.eq(Coffee.NAME)).getKey());
     }
 
     @Test
     public void testLogicalOperatorsWithoutPlaceholders() {
-        assertEquals("(name) = ('Espresso')", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso")));
-        assertEquals("(name) != ('Espresso')", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.neq("Espresso")));
-        assertEquals("(price) > (2.0)", SQLUtil.buildSqlWithoutPlaceholders(Coffee.PRICE.gt(2.0)));
-        assertEquals("((name) = ('Espresso')) AND ((price) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").and(Coffee.PRICE.gt(2.0))));
-        assertEquals("((name) = ('Espresso')) OR ((price) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").or(Coffee.PRICE.gt(2.0))));
-        assertEquals("((name) = ('Espresso')) XOR ((price) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").xor(Coffee.PRICE.gt(2.0))));
-        assertEquals("(NOT ((name) = ('Espresso')))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").not()));
+        assertEquals("(`name`) = ('Espresso')", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso")));
+        assertEquals("(`name`) != ('Espresso')", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.neq("Espresso")));
+        assertEquals("(`price`) > (2.0)", SQLUtil.buildSqlWithoutPlaceholders(Coffee.PRICE.gt(2.0)));
+        assertEquals("((`name`) = ('Espresso')) AND ((`price`) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").and(Coffee.PRICE.gt(2.0))));
+        assertEquals("((`name`) = ('Espresso')) OR ((`price`) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").or(Coffee.PRICE.gt(2.0))));
+        assertEquals("((`name`) = ('Espresso')) XOR ((`price`) > (2.0))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").xor(Coffee.PRICE.gt(2.0))));
+        assertEquals("(NOT ((`name`) = ('Espresso')))", SQLUtil.buildSqlWithoutPlaceholders(Coffee.NAME.eq("Espresso").not()));
         Instant instant = Instant.parse("2023-01-01T00:00:00Z");
         assertEquals("'2023-01-01T00:00:00Z'", SQLUtil.buildSqlWithoutPlaceholders(Expression.constant(instant)));
     }
